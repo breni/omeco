283602041d833eb21158b455019238c1c4951cd4
Karl-Philipp Wulfert <wulfert@tu-berlin.de>
2018-09-21 12:22:54 +0200

    feature: add first working version

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # odata-metadata-to-typescript-interfaces
+
+## Globally
+
+```
+npm install -g odata-metadata-to-typescript-interfaces
+odata-metadata-to-typescript-interfaces --help
+```
+
+## Cloned 
+
+```
+node --require ts-node/register src/cli.ts --help
+```
---
+++ src/cli.ts
@@ -0,0 +1,174 @@
+import * as commander from 'commander';
+import {existsSync, readFile, readFileSync, writeFile} from 'fs';
+import {join} from 'path';
+import {promisify} from 'util';
+import {convertableToString, parseString} from 'xml2js';
+
+const pkgJson = JSON.parse(readFileSync(join(__dirname, '..', 'package.json')).toString());
+
+/**
+ * Async version of readFile
+ */
+const asyncReadFile = promisify(readFile);
+
+/**
+ * Async version of writeFile
+ */
+const asyncWriteFile = promisify(writeFile);
+
+/**
+ * Async version of parseString
+ *
+ * @param xml XML to parse
+ */
+function asyncParseString(xml: convertableToString): Promise<any> {
+  return new Promise((resolve, reject) => {
+    parseString(xml, (err: any, result: any) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+/**
+ * Translate an OData type to a TypeScript type
+ *
+ * @param oDataType OData type to translate
+ * @param nullable Whether or not the type shall be nullable
+ */
+function translateType(oDataType: string, nullable: boolean): string {
+  let typeScriptType = '';
+
+  switch (oDataType) {
+    case 'Edm.String':
+    case 'Edm.Binary':
+      typeScriptType = 'string';
+      break;
+    case 'Edm.Int32':
+      typeScriptType = 'number';
+      break;
+    case 'Edm.DateTime':
+      typeScriptType = 'Date';
+      break;
+    case 'Edm.Boolean':
+      typeScriptType = 'boolean';
+      break;
+    default:
+      throw new Error('No translation for ' + oDataType);
+  }
+
+  if (nullable) {
+    typeScriptType += ' | null';
+  }
+
+  return typeScriptType;
+}
+
+/**
+ * Generate name from namespace and name
+ *
+ * @param namespace Namespace to generate name from
+ * @param name Name to generate name from
+ */
+function generateName(namespace: string, name: string): string {
+  return namespace + '_' + name;
+}
+
+commander
+  .command('convert <metadataXml> <interfacesDTs>')
+  .version(pkgJson.version)
+  .description('Convert OData metadata to TypeScript interfaces')
+  .action(async (metadataXml, interfacesDTs) => {
+    if (!existsSync(metadataXml)) {
+      throw new Error('File `' + metadataXml + '` does not exist!');
+    }
+
+    if (existsSync(interfacesDTs)) {
+      throw new Error('File `' + interfacesDTs + '` does exist!');
+    }
+
+    const buffer = await asyncReadFile(metadataXml);
+
+    const metadata = await asyncParseString(buffer);
+
+    const typeScriptInterfaces: any[] = [];
+
+    metadata['edmx:Edmx']['edmx:DataServices'].forEach((dataService: any) => {
+      dataService.Schema.forEach((schema: any) => {
+        schema.EntityType.forEach((entityType: any) => {
+          const typeScriptInterface: any = {
+            name: entityType.$.Name,
+            namespace: schema.$.Namespace,
+            properties: [],
+          };
+
+          entityType.Property.forEach((property: any) => {
+            typeScriptInterface.properties.push({
+              name: property.$.Name,
+              type: translateType(property.$.Type, property.$.Nullable),
+            });
+          });
+
+          typeScriptInterfaces.push(typeScriptInterface);
+        });
+
+        schema.Association.forEach((association: any) => {
+          const principalRole = association.ReferentialConstraint[0].Principal[0].$.Role;
+          const dependentRole = association.ReferentialConstraint[0].Dependent[0].$.Role;
+
+          let principal: any = {};
+          let dependent: any = {};
+
+          association.End.forEach((end: any) => {
+            if (end.$.Role === principalRole) {
+              principal = end.$;
+            }
+
+            if (end.$.Role === dependentRole) {
+              dependent = end.$;
+            }
+          });
+
+          [principal.namespace, principal.name] = principal.Type.split('.');
+          [dependent.namespace, dependent.name] = dependent.Type.split('.');
+
+          typeScriptInterfaces.forEach((typeScriptInterface) => {
+            if (typeScriptInterface.namespace === principal.namespace
+              && typeScriptInterface.name === principal.name) {
+              const property = {
+                name: association.$.Name,
+                type: generateName(dependent.namespace, dependent.name),
+              };
+
+              if (dependent.Multiplicity === '*') {
+                property.type += '[]';
+              }
+
+              property.type += ' | Promise<' + property.type + '>';
+
+              typeScriptInterface.properties.push(property);
+            }
+          });
+        });
+      });
+    });
+
+    let output = '';
+    typeScriptInterfaces.forEach((typeScriptInterface) => {
+      output += 'export interface ' + generateName(typeScriptInterface.namespace, typeScriptInterface.name) + ' {\n';
+
+      typeScriptInterface.properties.forEach((property: any) => {
+        output += '  ' + property.name + ': ' + property.type + ';\n';
+      });
+
+      output += '}\n\n';
+    });
+
+    await asyncWriteFile(interfacesDTs, output);
+  });
+
+commander
+  .parse(process.argv);
