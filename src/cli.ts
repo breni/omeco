036aae70c0828cbc756ea503ee22446c89897364
Karl-Philipp Wulfert <wulfert@tu-berlin.de>
2018-10-30 12:35:09 +0100

    feat: add possibility to sort interfaces and props

--- src/cli.ts
+++ src/cli.ts
@@ -18,6 +18,7 @@ commander
   .option('-d, --debug', 'Write JSON representation of XML to file')
   .option('-f, --force', 'Overwrite existing TypeScript definition file')
   .option('-l, --deferred [type]', 'Type of deferred to use')
+  .option('-s, --sort', 'Whether or not to sort interfaces and properties')
   .action(async (metadataXml, interfacesDTs, cmd) => {
     // check if supplied metadata file exists
     if (!existsSync(metadataXml)) {
@@ -50,6 +51,18 @@ commander
 
     // extract data
     const extractedData = await extractData(buffer.toString());
+
+    if (cmd.sort) {
+      extractedData.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+
+      extractedData.forEach((typeScriptInterface) => {
+        typeScriptInterface.properties.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        });
+      });
+    }
 
     // determine deferred type
     const deferredType = ['JQuery.Deferred', 'Promise'].indexOf(cmd.deferred) >= 0 ? cmd.deferred : undefined;
